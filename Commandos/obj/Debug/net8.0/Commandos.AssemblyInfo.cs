475a5a4b059b15b783c2495fae058e85f36182c3
moshik <moshik0527669169@gmail.com>
2025-05-27 14:58:06 +0300

    add enemy class

--- Commandos/obj/Debug/net8.0/Commandos.AssemblyInfo.cs
+++ Commandos/obj/Debug/net8.0/Commandos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Commandos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e01b250e64ca1312a3568d42b119d5dc95df104b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5d25ce7ddfdb1a40fafb78b18bf23de96614f92")]
 [assembly: System.Reflection.AssemblyProductAttribute("Commandos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Commandos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
