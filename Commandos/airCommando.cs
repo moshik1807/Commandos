fdfde911ceb7004eccf1312b6e66a4906d3b6014
moshik <moshik0527669169@gmail.com>
2025-05-27 11:39:03 +0300

    add air Commando

---
+++ Commandos/airCommando.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Xml.Linq;
+namespace comando
+{
+    public class AirCommando: Commando
+    {
+        public AirCommando(string Name, string NameCode, string[] Tools, string Stetus):base(string name, string nameCode, string[] tools, string status)
+        {
+        }
+        public void parachuting()
+        {
+            Console.WriteLine("The soldier falls");
+        }
+    }
+
+}
--- Commandos/obj/Debug/net8.0/Commandos.AssemblyInfo.cs
+++ Commandos/obj/Debug/net8.0/Commandos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Commandos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c82c6ce485f674d237657ec424dff9837a5603f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f78c2bb7832838765ea5e6dd37f4c6ab48a10790")]
 [assembly: System.Reflection.AssemblyProductAttribute("Commandos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Commandos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
